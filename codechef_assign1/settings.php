f725df91843ad6ed2bbf20c24c78cf0a3e3a5fa3
Abhinav Koppula <abhinav.koppula@gmail.com>
2014-06-20 18:44:19 +0530

    Pushed Assignment 1 files

---
+++ codechef_assign1/settings.php
@@ -0,0 +1,10 @@
+<?php
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "898364240-G3zhChRVefoRIzln52iUqT60xDqH6INKBfhX5wXo",
+    'oauth_access_token_secret' => "XHYiro9aSlweE1gayADsTrhCUdrQPDM1pIEqOl4iRlI",
+    'consumer_key' => "i9nouSAUAqyieRSjUCmw",
+    'consumer_secret' => "1jADCsxcRpdKad0KcN1iTlw56M0mYJTDkqVRHNDpTA"
+);
+
+?>
